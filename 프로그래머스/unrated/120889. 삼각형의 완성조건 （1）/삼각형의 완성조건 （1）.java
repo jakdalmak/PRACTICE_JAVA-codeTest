04413221abbb5b86c75738d51773b58a8fc4cf28
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2023-12-31 00:24:48 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 2, max = 0, sum = 0;
+        
+        // 1. 가장 큰 값을 배열 순회를 통해 구한다.
+        // 2. 나머지 두 값의 합 > 가장 큰 값 인 경우에만 삼각형을 제작할 수 있다.
+        
+        
+        // 배열 최댓값 얻기
+        for(int i = 0; i < sides.length; i++) {
+            if(max < sides[i]) max = sides[i];
+            sum += sides[i];
+        }
+        
+        // 삼각형 제작이 가능한 경우 1로 변환하여 반환
+        if(sum-max > max) answer = 1;
+        
+        return answer;
+    }
+}
