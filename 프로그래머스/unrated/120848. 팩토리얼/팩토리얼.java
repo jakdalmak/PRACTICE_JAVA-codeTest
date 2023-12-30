67303853e95e49bce62d4f3ebaf4d4b349e62a2c
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2023-12-31 00:58:09 +0900

    [level unrated] Title: 팩토리얼, Time: 0.02 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
@@ -2,16 +2,17 @@ import java.math.*;
 
 class Solution {
     public int solution(int n) {
-        int temp = 1, factorial = 1;
+        int temp = 1, factorial = 0;
         
         
         while(temp <= n) {
+            ++factorial;
             temp *= factorial;
-            factorial++;
+            
         }
         // 위처럼 수행 시, factorial 연산이 정답보다 한 번 더 수행되는 문제 있음.
 
-        return factorial - 2;
+        return factorial - 1;
     }
 
 }
