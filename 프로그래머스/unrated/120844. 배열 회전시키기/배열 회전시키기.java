1cb953614014276f01a7286ed827673a607874eb
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2023-12-31 00:18:32 +0900

    Update 배열 회전시키기.java

--- 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.java
@@ -2,13 +2,12 @@ class Solution {
     public int[] solution(int[] numbers, String direction) {
         int[] answer = new int[numbers.length];
         
-         
+        // 돌리는 방식은 같다. 단지, 시작하는 인덱스에 차이가 있을 뿐이다.
         if(direction.equals("left")) {
             for(int i = 1; i <= numbers.length; i++) 
                 answer[i - 1] = numbers[i % numbers.length];
         }
         
-        // right는 위같이 단순 수식으로는 어떻게 구현 해야할지 모르겠음..
         if(direction.equals("right")) {
             int i = numbers.length-1;
             for(int index = 0; index < numbers.length; index++)
