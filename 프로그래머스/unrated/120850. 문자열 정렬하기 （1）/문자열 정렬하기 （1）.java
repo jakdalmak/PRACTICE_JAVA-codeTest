8680b530e2e0ba4243158c5c9d2e7a5c59d16b86
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-01 18:36:24 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 0.71 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -4,6 +4,11 @@ class Solution {
     public int[] solution(String my_string) {
         // ASCII 코드를 활용하여, 9 >= char - '0' >= 0에 해당하는 문자들만 별도의 배열에 배치한다.
         // 해당 배열을 sort 하는 것으로 정렬을 수행한다.
+        
+        
+        // String 문제의 경우는 반드시 char[], string[]을 빌려야만 해결할 수 있는 경우가 아니다.
+        // String.charAt() 이라는 메소드를 절대 잊지 말자.
+        /*
         ArrayList<Integer> resultList = new ArrayList<>();
 
         char[] arr = my_string.toCharArray();
@@ -17,6 +22,18 @@ class Solution {
         Collections.sort(resultList);
         int[] answer = new int[resultList.size()];
         for(int i = 0; i < resultList.size(); i++) answer[i] = resultList.get(i);
+        */
+        
+        my_string = my_string.replaceAll("[a-z]", "");
+        
+        int[] answer = new int[my_string.length()];
+        // replaceAll을 수행한 이후의 크기를 기준으로 결과 배열을 생성하기.
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            answer[i] = my_string.charAt(i) - '0';
+        }
+        
+        Arrays.sort(answer);
         
         return answer;
     }
