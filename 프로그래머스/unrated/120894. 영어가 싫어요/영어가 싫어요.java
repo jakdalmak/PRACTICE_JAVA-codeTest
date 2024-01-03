8b2f4c73c2374e12c55c7148d7f4e420aac5095d
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-04 00:21:17 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
@@ -2,6 +2,19 @@ import java.util.*;
 
 class Solution {
     public long solution(String numbers) {
+        // 본 문제는 '해시' 분류의 문제므로, 해시맵을 사용해서 풀어보기.
+        Map<String, String> map = Map.of("zero", "0", "one", "1", "two", "2", 
+                                         "three", "3", "four", "4", "five", "5",
+                                         "six", "6", "seven", "7", "eight", "8", "nine", "9");
+        
+        for(String num : map.keySet()) {
+            numbers = numbers.replace(num, map.get(num));
+        }
+        
+        return Long.parseLong(numbers);
+        
+        
+        /*
         numbers = numbers.replace("zero", "0");
         numbers = numbers.replace("one", "1");
         numbers = numbers.replace("two", "2");
@@ -14,6 +27,9 @@ class Solution {
         numbers = numbers.replace("nine", "9");
         
         long answer = Long.parseLong(numbers);
+        
         return answer;
+        */
+        
     }
 }
