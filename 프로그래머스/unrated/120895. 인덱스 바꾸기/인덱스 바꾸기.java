2ef9eae98a4ed3a8ce342a1ea77573fa5e46d4be
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-04 19:17:11 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 1.71 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        // 각 문자를 바꾸어야 하므로, String[], char[] 등으로 바꾸어 변경해야한다.
+        // 그러나, 본 문제는 해시를 이용하는 것을 권장하므로, 해시맵을 이용하여 해보기.
+        
+        Map<Integer, Character> map = new HashMap<>();
+
+        for(int i = 0; i < my_string.length(); i++) {
+            map.put(i, my_string.charAt(i));
+        }
+
+        char temp = map.get(num1);
+        map.replace(num1, map.get(num1), map.get(num2));
+        map.replace(num2, map.get(num2), temp);
+
+        String answer = "";
+        
+        for(int i = 0; i < map.size(); i++) answer += map.get(i);
+        return answer;
+    }
+}
