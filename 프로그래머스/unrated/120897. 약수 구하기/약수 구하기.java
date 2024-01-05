23b19df5711f6b2a8f7427a8caae367fcd8308a1
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-05 21:02:05 +0900

    [level unrated] Title: 약수 구하기, Time: 2.17 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++) if(n % i == 0) list.add(i);
+        
+        Collections.sort(list);
+        
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        
+        
+        return answer;
+    }
+}
