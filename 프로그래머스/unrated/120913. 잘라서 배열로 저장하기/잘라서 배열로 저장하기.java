0e01a9e67a8e0eff71d1af1c0d81a6714a7d959a
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-07 20:08:35 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.03 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -4,6 +4,7 @@ class Solution {
         // replaceFirst는 처음 만난 값부터 없앤다는 것 이용하기.
         
         /* 첫 번째 제출 답안. */
+        /*
         String[] answer = new String[my_str.length() % n != 0 ?
                                      my_str.length() / n + 1 : my_str.length() / n];
         
@@ -14,7 +15,22 @@ class Solution {
         
         // 마지막번째는 0 ~ n까지 불가능하다. n까지 존재하는지 확실치 않음.
         answer[answer.length - 1] = my_str.substring(0);
+        */
         
+        /* 두 번째 제출 답안 */
+        // 차이점 : 슬라이딩 윈도우를 사용.
+        // 첫 번째 정답의 경우, string을 추가적으로 다듬는 replaceFirst 가 들어가서
+        // 불필요한 연산을 추가적으로 수행하고 있다.
+        
+        String[] answer = new String[my_str.length() % n != 0 ?
+                                     my_str.length() / n + 1 : my_str.length() / n];
+        
+        for(int i = 0; i < answer.length; i++) {
+            int start = n * i;
+            int end = my_str.length() <= n * (i + 1) ? my_str.length() : n * (i + 1);
+            
+            answer[i] = my_str.substring(start, end);
+        }
         
         return answer;
     }
