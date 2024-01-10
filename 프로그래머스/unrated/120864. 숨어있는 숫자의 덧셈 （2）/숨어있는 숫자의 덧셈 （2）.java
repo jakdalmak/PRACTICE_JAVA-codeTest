01ef1f4182057d45b20fd65f1f3ae570af85208b
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-10 18:24:48 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.22 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,10 +1,14 @@
 class Solution {
     public int solution(String my_string) {
-        int answer = 0;
+        
         
         // my_string = my_string.replaceAll("[A-z]", " ").trim();
         // => 숫자 사이에 공백이 여러 칸 들어가게 되므로 split 불가능.
+        // => regex의 사용 방식이 틀렸다! 공백 하나로 치환 되는 것이 맞음 => 2번 답안으로.
         
+        /* 첫 번째 제출 답안 */
+        /*
+        int answer = 0;
         int Num = 0;
         for(int i = 0; i < my_string.length(); i++) {
             // 숫자를 만나면 Num 첫 값 할당
@@ -27,6 +31,19 @@ class Solution {
         }
         answer += Num; // 마지막까지 집계된 Num을 추가해주기
 
+        return answer;
+        */
+        
+        
+        /* 두 번째 제출 답안 */
+        // " "와 같이 공백을 기준으로 split을 수행하게 되는 경우,
+        // "123" "" "" "" "" "2323"... 과 같이 비어있는 문자열값을 배열이 지니게 된다.
+        // 즉, 이 비어있는 문자열 값만 별도로 구분한다면, replaceAll - split 연계로 충분히 풀 수 있었던 문제.
+        String[] splitStr = my_string.replaceAll("[a-zA-Z]", " ").trim().split(" ");
+    
+        int answer = 0;
+        
+        for(String str : splitStr) if(!(str.equals("")))answer += Integer.parseInt(str);
         
         return answer;
     }
