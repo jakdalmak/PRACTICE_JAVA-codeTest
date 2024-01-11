08dff68551c078541454edd1988565d1f77333e9
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-12 00:22:23 +0900

    [level Lv.0] Title: 저주의 숫자 3, Time: 0.08 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/Lv.0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -7,22 +7,15 @@ class Solution {
         
         // 이때, 3이 들어간 숫자의 경우, 1자리수씩 확인할 경우 for 반복문을 더 사용해야 하므로
         // 현재 숫자를 String으로 변환한 뒤, contains()를 사용하여 3이 존재하는지 확인하는 식으로 수행하자.
-        
-        int answer = 1;
-        int index = 0;
-        while (index < n) {
-            if(answer % 3 == 0 || String.valueOf(answer).contains("3")) {
-                answer++;
-                continue; 
-                // 23 24와 같이, 3이 들어간 수 이후에 3의 배수가 나올 수 있음.
-                // 마찬가지로, 12 13과 같이 3의 배수 이후에 3이 들어간 수가 나올 수 있음.
-                // 이로 인해 값이 증가하면 조건식을 다시 확인하는 절차가 필요함. 이를 위해 continue 사용.
-            } 
-            index++;
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
             answer++;
+            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                answer++;
+            }
         }
-        
-        
-        return answer-1;
+        return answer;
     }
 }
