2105f5e40629b51e800677c511c9d6dd7cebec25
jakdalmak <154880100+jakdalmak@users.noreply.github.com>
2024-01-19 23:11:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.37 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -27,5 +27,25 @@ class Solution {
         }
         
         return "";
+        
+        
+        
+        /** 다른 사람의 답안 */
+        /*
+        String answer = "";
+        
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        
+        return answer;
+        */
     }
 }
